a31ad144a5faaa591041384d74da3f3955b85a2a
Jakdar Alabdi <jakdaralabdi@gmail.com>
2025-11-10 23:27:11 +0100

    Solving some LP with Gurobi and JuMP

--- dev/main.jl
+++ dev/main.jl
@@ -1,4 +1,6 @@
 using VeryDiff
+using LinearAlgebra
+using JuMP, Gurobi
 
 VeryDiff.NEW_HEURISTIC = false
 
@@ -37,8 +39,39 @@ N = GeminiNetwork(N1, N2)
 input_range = max(eps, 3.0 * rand())
 
 Zin = Zonotope(Matrix(input_range * I, input_dim, input_dim), rand(input_dim), nothing)
+prop_state = PropState(true)
 
-println("Verify epsilon property:")
-property = get_epsilon_property(eps)
-input_split_heuristic = epsilon_split_heuristic
+L1 = N.network1.layers
+L2 = N.network2.layers
+∂L = N.diff_network.layers
+
+∂G = Matrix(1.0I, 2, 3)
+mul!((@view ∂G[:, 1:2]), [2.0 0; 0.0 2], [2.0 0; 0.0 2])
+
+Z = Zonotope(Matrix(input_range * I, input_dim, input_dim), rand(input_dim), nothing)
+
+bounds = zono_bounds(Z)
+lower = bounds[:, 1]
+upper = bounds[:, 2]
+
+split_index = argmax((i -> ifelse(lower[i] < 0 < upper[i], upper[i] - lower[i], 0)), 1:size(bounds, 1))
+model = JuMP.Model(() -> Gurobi.Optimizer(VeryDiff.GRB_ENV[]))
+set_time_limit_sec(model, 10)
+
+g = copy(Z.G[split_index, :])
+c = copy(Z.c[split_index])
+var_num = size(Z.G, 2)
+
+@variable(model, -1.0 <= x[1:var_num] <= 1.0)
+@constraint(model, g ⋅ x + c <= 0.0)
+@objective(model, Max, x)
+
+
+optimize!(model)
+
+objective_value(model)
+
+# println("Verify epsilon property:")
+# property = get_epsilon_property(eps)
+# input_split_heuristic = epsilon_split_heuristic
 
