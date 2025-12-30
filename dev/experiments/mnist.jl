19be416c3128bb5d5cc0a4b34f87a387feab281f
uzibq <uzibq@student.kit.edu>
2025-12-30 01:57:46 +0100

    New approach to run VeryDiff with MNIST instances on the Poll server

--- dev/experiments/mnist.jl
+++ dev/experiments/mnist.jl
@@ -1,12 +1,8 @@
 cur_dir = @__DIR__
 benchmarks_dir = "$cur_dir/../../../verydiff-experiments"
 
-# csv_dir = "$cur_dir/mnist-prune.csv"
-
-function load_specs(num::Int)
-    mnist_specs = SpecificationEpsilon[]
-    csv_dir = "$cur_dir/mnist-prune-verydiff-$num.csv"
-    open(csv_dir, "r") do f
+function _run_mnist_all(specs_csv_file::String, log_dir::String, run_name::String, eval_func)
+    open(specs_csv_file, "r") do f
         while !eof(f)
             spec = split(readline(f), ",")
             nn_file₁ = "$benchmarks_dir/$(spec[1])"
@@ -14,41 +10,32 @@ function load_specs(num::Int)
             spec_file = "$benchmarks_dir/$(spec[3])"
             epsilon = parse(Float64, string(spec[4]))
             timeout = parse(Int64, string(spec[5]))
-            push!(mnist_specs, SpecificationEpsilon(nn_file₁, nn_file₂, spec_file, epsilon, timeout))
-        end
-    end
-    return mnist_specs
-end
 
-
-function _run_mnist_all(specs::Vector{SpecificationEpsilon}, log_dir::String, run_name::String, eval_func)
-    for spec in specs
-        (; nn_file₁, nn_file₂, spec_file, epsilon, timeout) = spec
-        net_name = replace(basename(nn_file₂), ".onnx" => "", "mnist_relu_" => "")
-        out_dir = joinpath(log_dir, run_name, "mnist-$epsilon", net_name)
-        mkpath(out_dir)
-        spec_file_name = replace(basename(spec_file), ".vnnlib" => "")
-        log_file_name = joinpath(out_dir, "$spec_file_name.log")
-        csv_file_name = joinpath(log_dir, run_name, "mnist-$epsilon", "results.csv")
-        
-        original_stdout = stdout
-        original_stderr = stderr
-        open(log_file_name, "w") do f
-            redirect_stdout(f)
-            redirect_stderr(f)
-            flush(stdout)
-            flush(stderr)
-            eval_func(spec, csv_file_name)
-            flush(stdout)
-            flush(stderr)
-            GC.gc()
-            redirect_stdout(original_stdout)
-            redirect_stderr(original_stderr)
+            net_name = replace(basename(nn_file₂), ".onnx" => "", "mnist_relu_" => "")
+            out_dir = joinpath(log_dir, run_name, "mnist-$epsilon", net_name)
+            mkpath(out_dir)
+            spec_file_name = replace(basename(spec_file), ".vnnlib" => "")
+            log_file_name = joinpath(out_dir, "$spec_file_name.log")
+            csv_file_name = joinpath(log_dir, run_name, "mnist-$epsilon", "results.csv")
+            
+            original_stdout = stdout
+            original_stderr = stderr
+            open(log_file_name, "w") do f
+                redirect_stdout(f)
+                redirect_stderr(f)
+                flush(stdout)
+                flush(stderr)
+                eval_func(nn_file₁, nn_file₂, spec_file, epsilon, timeout, csv_file_name)
+                flush(stdout)
+                flush(stderr)
+                GC.gc()
+                redirect_stdout(original_stdout)
+                redirect_stderr(original_stderr)
+            end
         end
     end
 end
 
-function run_mnist_all(eval_func, run_name::String, num::Int)
-    # _run_mnist_all(mnist_specs, "$cur_dir\\experiments_final", run_name, eval_func)
-    _run_mnist_all(load_specs(num), "$cur_dir/experiments_final", run_name, eval_func)
+function run_mnist_all(eval_func, run_name::String)
+    _run_mnist_all("$cur_dir\\mnist-prune.csv", "$cur_dir\\experiments_final", run_name, eval_func)
 end
--- dev/experiments/run.jl
+++ dev/experiments/run.jl
@@ -6,9 +6,7 @@ function parse_networks(nn_file₁::String, nn_file₂::String)
     return N₁, N₂
 end
 
-function save_results(out_dir::String, spec::SpecificationEpsilon, status::VeryDiff.VerificationStatus, δ_bounds::Tuple{Float64, Float64}, to::TimerOutputs.TimerOutput)
-    net_name = replace(basename(spec.nn_file₂), ".onnx" => "")
-    spec_name = replace(basename(spec.spec_file), ".vnnlib" => "")
+function save_results(out_dir::String, net_name::String, spec_name::String, status::VeryDiff.VerificationStatus, δ_bounds::Tuple{Float64, Float64}, to::TimerOutputs.TimerOutput)
     runtime, num_propagations, num_input_splits, num_neuron_splits = 0, 0, 0, 0
     if "Verify" in keys(to.inner_timers)
         inner = to["Verify"]
@@ -48,8 +46,7 @@ function save_results(out_dir::String, spec::SpecificationEpsilon, status::VeryD
     end
 end
 
-function verydiff(spec::SpecificationEpsilon, result_out_dir::String)
-    (;nn_file₁, nn_file₂, spec_file, epsilon, timeout) = spec
+function verydiff(nn_file₁::String, nn_file₂::String, spec_file::String, epsilon::Float64, timeout::Int64, result_out_dir::String)
     N₁, N₂ = parse_networks(nn_file₁, nn_file₂)
     f, n_inputs, _ = get_ast(spec_file)
     property_check = get_epsilon_property(epsilon)
@@ -57,25 +54,28 @@ function verydiff(spec::SpecificationEpsilon, result_out_dir::String)
     set_deepsplit_config((false, false, false, false))
     for (bounds, _, _, _) in f
         status, δ_bounds = verify_network(N₁, N₂, bounds, property_check, epsilon_split_heuristic; timeout=timeout)
-        save_results(result_out_dir, spec, status, δ_bounds, VeryDiff.to)
+        net_name = replace(basename(nn_file₂), ".onnx" => "")
+        spec_name = replace(basename(spec_file), ".vnnlib" => "")
+        save_results(result_out_dir, net_name, spec_name, status, δ_bounds, VeryDiff.to)
     end
 end
 
 function deepsplit(config::Tuple{Bool, Bool, Bool, Bool})
-    return (spec::SpecificationEpsilon, result_out_dir::String) -> begin
-        (;nn_file₁, nn_file₂, spec_file, epsilon, timeout) = spec
+    return (nn_file₁::String, nn_file₂::String, spec_file::String, epsilon::Float64, timeout::Int64, result_out_dir::String) -> begin
         N₁, N₂ = parse_networks(nn_file₁, nn_file₂)
         f, n_inputs, _ = get_ast(spec_file)
         println("Using DeepSplit...")
         set_deepsplit_config(config)
         for (bounds, _, _, _) in f
             status, δ_bounds = deepsplit_lp_search_epsilon(N₁, N₂, bounds, epsilon; timeout=timeout)
-            save_results(result_out_dir, spec, status, δ_bounds, VeryDiff.to)
+            net_name = replace(basename(nn_file₂), ".onnx" => "")
+            spec_name = replace(basename(spec_file), ".vnnlib" => "")
+            save_results(result_out_dir, net_name, spec_name, status, δ_bounds, VeryDiff.to)
         end
     end
 end
 
-function run_experiments(num::Int)
+function run_experiments()
     # println("Running acas all...")
     
     # println("\nRunning on VeryDiff")
@@ -99,7 +99,7 @@ function run_experiments(num::Int)
 
     println("Running mnist all...")
     println("\nRunning on VeryDiff")
-    run_mnist_all(verydiff, "VeryDiff-$num", num)
+    run_mnist_all(verydiff, "VeryDiff")
     # println("\nRunning on DeepSplit-Base")
     # run_mnist_all(deepsplit((true, false, false, false)), "DeepSplit-Base")
     # println("\nRunning on DeepSplit-Alt")
